d835138e38fec1a63421047a38c8e52a16dc35b4
Maxim Kemajou <maxim.kemajou@blue-zone.io>
2024-01-27 16:23:18 +0100

    add a middleware and a function to hanlder errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   //console.log(req.body);
 
   const { username, email, password } = req.body;
 
   if (
     !username || !email ||!password || !username === "" || !email === "" ||!password ) {
-    return res.status(400).json({ message: "all fields required" });
+    // return res.status(400).json({ message: "all fields required" });
+    next(errorHandler(400, "All fields are required"));
   }
-
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -25,6 +26,7 @@ export const signup = async (req, res) => {
 
     res.json({ message: "signup successful" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    //res.status(500).json({ message: error.message });
+    next(error);
   }
 };
