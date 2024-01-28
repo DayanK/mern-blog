5646d972b4f8b0ef0572d3a6469c8691f94a9e55
Maxim Kemajou <maxim.kemajou@blue-zone.io>
2024-01-28 15:08:27 +0100

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
  function App() {
   return (
@@ -21,6 +22,7 @@ import Header from './components/Header';
         <Route path='/projects' element={<Projects />}/>
 
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
